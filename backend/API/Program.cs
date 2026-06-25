1ba8fb4d6df24a482b7cef80872f595bfe68648e
Nikitososs <nsmirnow.n@gmail.com>
2026-06-25 12:31:58 +0600

    feat: add API.http test requests and configure string enums

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -4,7 +4,11 @@ using API.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
 
 // Configure EF Core DbContext with SQL Server
 builder.Services.AddDbContext<AppDbContext>(options =>
