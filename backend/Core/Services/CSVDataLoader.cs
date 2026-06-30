4ab546677d061985baa2edbc0d0b09630c85f48f
Olesya <olegha2000@gmail.com>
2026-06-30 07:15:32 +0600

    feat: time service implementation + minor mode fixes

--- backend/Core/Services/CSVDataLoader.cs
+++ backend/Core/Services/CSVDataLoader.cs
@@ -1,22 +1,22 @@
 using Core.Models;
 using Core.Interfaces;
-using Sep;
 using nietras.SeparatedValues;
 using Microsoft.VisualBasic;
+using System;
 
 namespace Core.Services;
 
 
 public class CSVDataLoader : IDataLoader
 {
-    public var _file = null;
+    private string _file = null;
 
-    public CSVDataLoader(var file)
+    public CSVDataLoader(string file)
     {
         _file = file;
     }
 
-    IEnumerable<CallRecord> LoadData()
+    public IEnumerable<CallRecord> LoadData()
     {
         using var reader = Sep.New(',').Reader().FromFile(_file);
 
@@ -30,9 +30,6 @@ public class CSVDataLoader : IDataLoader
             yield return new CallRecord
             {
                 Timestamp = DateTime.Parse(row["Timestamp"].Span),
-                UserId = row["UserId"].ToString(),
-                CallType = row["CallType"].ToString(),
-                DurationSeconds = row["DurationSeconds"].Parse<int>()
             };
         }
     }
