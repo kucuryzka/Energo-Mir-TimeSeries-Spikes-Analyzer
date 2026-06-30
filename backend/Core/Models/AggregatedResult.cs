ef5b6ead28a75f7576af1590b7a575e4fc2fa2bc
Nikitososs <nsmirnow.n@gmail.com>
2026-06-30 16:13:20 +0600

    perf: Optimize date range aggregation by offloading to SQL Server and remove 365-day limit

---
+++ backend/Core/Models/AggregatedResult.cs
@@ -0,0 +1,8 @@
+namespace Core.Models;
+
+public class AggregatedResult
+{
+    public DateTime Timestamp { get; set; }
+    public int Value { get; set; }
+    public int ChannelId { get; set; }
+}
